658fa32093df277e11e08c6e0858f9951cd8c067
Unknown <kwangkg.30@gmail.com>
2017-12-15 14:12:51 +0700

    ข้อ 7_1

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -3,25 +3,14 @@
 using namespace std;
 
 int main() {
-  
+
   cout << "This is simple loop"<< endl << endl ;
 
   int i = 0;
 
-  // ศึกษาตัวอย่างการใช้ for
-  for(i = 0; i < 10 ; i++) {
-    cout << "Round " << i << endl;
-  }
-
   // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
-  cout << "Hello World 1" << endl;
-  cout << "Hello World 2" << endl;
-  cout << "Hello World 3" << endl;
-  cout << "Hello World 4" << endl;
-  cout << "Hello World 5" << endl;
-  cout << "Hello World 6" << endl;
-  cout << "Hello World 7" << endl;
-  cout << "Hello World 8" << endl;
-
+   for(i = 0; i < 100 ; i++) {
+    cout << "Hello World " << i << endl;
+   }
   return 0;
 }
